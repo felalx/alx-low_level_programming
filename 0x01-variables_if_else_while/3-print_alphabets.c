6304a372a0a71599441388631b44850a521b3b14
root <Felix Ozoh>
2023-05-22 16:14:45 -0700

    great

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
-
 /**
- * main - Program that prints the alphabet in lowercase, and then in,
- * uppercase, followed by a new line.
- *
- * You can only use the putchar function (every other function (printf,
- * puts, etc…) is forbidden).
- * All your code should be in the main function.
- * You can only use putchar three times in your code.
- *
- * Return: 0
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
 */
-
 int main(void)
 {
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-		putchar('\n');
-	return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
