8b44bcd516a8fad34a7adece19c67e385882eef3
root <Felix Ozoh>
2023-05-25 08:17:20 -0700

    great

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * _isdigit - checks if parameter is a number between 0 to 9.
- * @c: input number.
- * Return: 1 if is a number (0 to 9), 0 in other case.
- */
+* _isdigit - returns 1 if digit, 0 if not
+* @c: an int given by the main function
+*
+* Description: Uses headers to link and a nested loops to achieve goal
+* Return: see first line
+*/
+
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
