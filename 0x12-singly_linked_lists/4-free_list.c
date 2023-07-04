776e216ea4a1fbf3e99ff7e75ae951f752d5a8c2
root <Felix Ozoh>
2023-07-03 18:53:44 -0700

    Hot fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
- /**
-  * free_list - Frees a list_t
-  * @head: A pointer to the list_t
-  * Return:void
-  */
+/**
+ * free_list - function that frees a singly linked list.
+ * @head: pointer to singly linked list.
+ *
+ * Return: no return.
+ *
+ */
+
 void free_list(list_t *head)
 {
-	list_t *check, *c;
+	list_t *temp_variable;
+	/* assign temp_variable to head */
+	temp_variable = head;
 
-c = head;
-while (c != NULL)
-{
-	check = c->next;
-	free(c->str);
-	free(c);
-	c = check;
-}
-
+	/* check if head is NULL if yes the list is empty and we just return */
+	while (head != NULL)
+	{
+		/* save the head in a temp variable */
+		temp_variable = head;
+		/* make the head point to the next node on list */
+		head = head->next;
+		/* free the str */
+		free(temp_variable->str);
+		/* free temp_variable and head just points to the rest of the list */
+		free(temp_variable);
+	}
+	/* free the head */
+	free(head);
 }
